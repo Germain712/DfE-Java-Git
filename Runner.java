cca4d0b3cdae27762b22de97c9a8d5c9193dd90a
Germain712 <Germain712@outlook.com>
2022-06-15 16:53:11 +0100

    Add files via upload

---
+++ Runner.java
@@ -0,0 +1,11 @@
+package com.qa.constructors;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		//DataType variableName = value
+		Artist = new Artist("Madonna", "Ray of Light", 1998);
+
+	}
+
+}
