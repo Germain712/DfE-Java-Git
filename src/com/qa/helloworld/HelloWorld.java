31785ec3be4f87714dab7325b6485c1463cb2498
Germain712 <Germain712@outlook.com>
2022-06-27 14:40:45 +0100

    Update HelloWorld.java

--- src/com/qa/helloworld/HelloWorld.java
+++ src/com/qa/helloworld/HelloWorld.java
@@ -3,7 +3,8 @@ package com.qa.helloworld;
 public class HelloWorld {
 
 public static void main(String[] args) {
-	System.out.println("Hello World!");
+	String message = "Hello World!";
+	System.out.println(message);
 }
 
 }
